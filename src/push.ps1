a6d95b2fc2c3dbe58b62d7d8c430527b7988da17
Taylor Ekle <eklet@dataio.com>
2023-10-27 11:29:36 -0700

    Attempt to fix CI build issue (#8)

--- src/push.ps1
+++ src/push.ps1
@@ -3,7 +3,7 @@ $artifacts = "./artifacts"
 
 Get-ChildItem $artifacts -Filter "*.nupkg" | ForEach-Object {
     Write-Host "$($scriptName): Pushing $($_.Name)"
-    dotnet nuget push "$artifacts/$_" --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+    dotnet nuget push $_ --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
     if ($lastexitcode -ne 0) {
         throw ("Exec: " + $errorMessage)
     }
