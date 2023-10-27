5a974119267a70233e60107485922ae6c04c0d2e
Taylor Ekle <eklet@dataio.com>
2023-10-27 09:33:08 -0700

    Add release build action
    Separate CI and PR builds
    Fix docs to match implementation of MachineType
    Add script to push to Nuget.org
    Rename PlacePart to PlacePartMessage for consistency

---
+++ src/push.ps1
@@ -0,0 +1,10 @@
+$scriptName = $MyInvocation.MyCommand.Name
+$artifacts = "./artifacts"
+
+Get-ChildItem $artifacts -Filter "*.nupkg" | ForEach-Object {
+    Write-Host "$($scriptName): Pushing $($_.Name)"
+    dotnet nuget push "$artifacts/$_" --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
