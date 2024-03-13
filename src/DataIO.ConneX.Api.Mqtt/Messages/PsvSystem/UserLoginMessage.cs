f87ca03eb19090e061b1776a34e15d44620cb462
Morgan Du Bois <DuBois@dataio.com>
2024-03-13 13:24:22 -0700

    Added 'UserLoginMessage.cs' file

---
+++ src/DataIO.ConneX.Api.Mqtt/Messages/PsvSystem/UserLoginMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace DataIO.ConneX.Api.Mqtt.Messages.PsvSystem
+{
+    /// <summary>
+    /// Class representing a User Login MQTT message
+    /// </summary>
+    public class UserLoginMessage
+    {
+        /// <summary>
+        /// Gets or sets whether or not the login was successful
+        /// </summary>
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the user who logged in
+        /// </summary>
+        public string Username { get; set; }
+    }
+}
